c4c22bf4b9be4ff2db287f219f97c90b9c798000
Marius Waldhorst <mpwaldhorst@hotmail.com>
2017-07-20 13:56:57 +0200

    Added unit tests for StringHelper. Also including some multibyte character tests.

--- src/Rehyved/Utilities/StringHelper.php
+++ src/Rehyved/Utilities/StringHelper.php
@@ -5,33 +5,36 @@ namespace Rehyved\Utilities;
 
 class StringHelper
 {
-    public static function endsWith(string $haystack, string $needle, bool $caseSensitive = true){
-        $haystackLength = strlen($haystack);
-        $needleLength = strlen($needle);
-        if ($needleLength > $haystackLength) return false;
-        return substr_compare($haystack, $needle, $haystackLength - $needleLength, $needleLength, $caseSensitive) === 0;
-    }
-
-    public static function equals($first, $second, bool $caseSensitive = true){
-        if($caseSensitive){
-            return $first === $second;
-        }else{
-            return mb_strtolower($first) === mb_strtolower($second);
-        }
-    }
-
-    public static function startsWith($haystack, $needle, bool $caseSensitive = true)
+    public static function endsWith(string $haystack, string $needle, bool $caseInsensitive = false): bool
     {
         $haystackLength = strlen($haystack);
         $needleLength = strlen($needle);
         if ($needleLength > $haystackLength) return false;
-        return substr_compare($haystack, $needle, 0, $needleLength, $caseSensitive) === 0;
+        return substr_compare($haystack, $needle, $haystackLength - $needleLength, $needleLength, $caseInsensitive) === 0;
     }
 
-    public static function contains($haystack, $needle, bool $caseSensitive = true){
-        if(!$caseSensitive){
+    public static function startsWith($haystack, $needle, bool $caseInsensitive = false) : bool
+    {
+        $haystackLength = strlen($haystack);
+        $needleLength = strlen($needle);
+        if ($needleLength > $haystackLength) return false;
+        return substr_compare($haystack, $needle, 0, $needleLength, $caseInsensitive) === 0;
+    }
+
+    public static function equals($first, $second, bool $caseInsensitive = false) : bool
+    {
+        if (!$caseInsensitive) {
+            return $first === $second;
+        } else {
+            return mb_strtolower($first) === mb_strtolower($second);
+        }
+    }
+
+    public static function contains($haystack, $needle, bool $caseInsensitive = false) : bool
+    {
+        if ($caseInsensitive) {
             return stripos($haystack, $needle) !== false;
         }
         return strpos($haystack, $needle) !== false;
-}
+    }
 }
---
+++ tests/Rehyved/Utilities/Mapper/StringHelperTest.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Rehyved\Utilities\Mapper;
+
+
+use PHPUnit\Framework\TestCase;
+use Rehyved\Utilities\StringHelper;
+
+class StringHelperTest extends TestCase
+{
+    public function testEndsWithCaseSensitive()
+    {
+        $testString = "testString";
+
+        $this->assertTrue(StringHelper::endsWith($testString, "String"));
+        $this->assertFalse(StringHelper::endsWith($testString, "string"));
+        $this->assertFalse(StringHelper::endsWith($testString, "garbage"));
+        $this->assertFalse(StringHelper::endsWith($testString, "test"));
+    }
+
+    public function testEndsWithCaseInsensitive()
+    {
+        $testString = "testString";
+
+        $this->assertTrue(StringHelper::endsWith($testString, "String"), true);
+        $this->assertTrue(StringHelper::endsWith($testString, "string", true));
+        $this->assertFalse(StringHelper::endsWith($testString, "garbage", true));
+        $this->assertFalse(StringHelper::endsWith($testString, "test", true));
+    }
+
+    public function testEndsWithWithMultiByteCharacters(){
+        $testString = "testStrïng";
+
+        $this->assertTrue(StringHelper::endsWith($testString, "Strïng"));
+        $this->assertTrue(StringHelper::endsWith($testString, "strïng", true));
+    }
+
+    public function testStartsWithCaseSensitive()
+    {
+        $testString = "testString";
+
+        $this->assertTrue(StringHelper::startsWith($testString, "test"));
+        $this->assertFalse(StringHelper::startsWith($testString, "Test"));
+        $this->assertFalse(StringHelper::startsWith($testString, "garbage"));
+        $this->assertFalse(StringHelper::startsWith($testString, "String"));
+    }
+
+    public function testStartsWithCaseInsensitive()
+    {
+        $testString = "testString";
+
+        $this->assertTrue(StringHelper::startsWith($testString, "test"));
+        $this->assertTrue(StringHelper::startsWith($testString, "Test", true));
+        $this->assertFalse(StringHelper::startsWith($testString, "garbage", true));
+        $this->assertFalse(StringHelper::startsWith($testString, "String", true));
+    }
+
+    public function testStartsWithWithMultiByteCharacters(){
+        $testString = "tëstString";
+
+        $this->assertTrue(StringHelper::startsWith($testString, "tëst"));
+        $this->assertTrue(StringHelper::startsWith($testString, "Tëst", true));
+    }
+
+    public function testEqualsCaseSensitive()
+    {
+        $testString = "testString";
+
+        $this->assertTrue(StringHelper::equals($testString, $testString));
+        $this->assertFalse(StringHelper::equals($testString, strtoupper($testString)));
+        $this->assertFalse(StringHelper::equals($testString, "garbage"));
+    }
+
+    public function testEqualsCaseInsensitive()
+    {
+        $testString = "testString";
+
+        $this->assertTrue(StringHelper::equals($testString, $testString), true);
+        $this->assertTrue(StringHelper::equals($testString, strtoupper($testString), true));
+        $this->assertFalse(StringHelper::equals($testString, "garbage", true));
+    }
+
+    public function testEqualsWithMultiByteCharacters(){
+        $testString = "tëstStrïng";
+
+        $this->assertTrue(StringHelper::equals($testString, $testString));
+        $this->assertTrue(StringHelper::equals($testString, strtoupper($testString), true));
+    }
+
+    public function testContainsCaseSensitive()
+    {
+        $testString = "testString";
+
+        $this->assertTrue(StringHelper::contains($testString, $testString));
+        $this->assertTrue(StringHelper::contains($testString, substr($testString, 2, strlen($testString) - 2)));
+        $this->assertFalse(StringHelper::contains($testString, strtoupper(substr($testString, 2, strlen($testString) - 2))));
+        $this->assertFalse(StringHelper::contains($testString, "garbage"));
+        $this->assertFalse(StringHelper::contains($testString, $testString . "garbage"));
+    }
+
+    public function testContainsCaseInsensitive()
+    {
+        $testString = "testString";
+
+        $this->assertTrue(StringHelper::contains($testString, $testString, true));
+        $this->assertTrue(StringHelper::contains($testString, substr($testString, 2, strlen($testString) - 2), true));
+        $this->assertTrue(StringHelper::contains($testString, strtoupper(substr($testString, 2, strlen($testString) - 2)), true));
+        $this->assertFalse(StringHelper::contains($testString, "garbage", true));
+        $this->assertFalse(StringHelper::contains($testString, $testString . "garbage", true));
+    }
+
+    public function testContainsWithMultiByteCharacters(){
+        $testString = "tëstStrïng";
+
+        $this->assertTrue(StringHelper::contains($testString, substr($testString, 2, strlen($testString) - 2)));
+        $this->assertTrue(StringHelper::contains($testString, strtoupper(substr($testString, 2, strlen($testString) - 2)), true));
+    }
+}
